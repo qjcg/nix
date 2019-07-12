4bc513bf06102525b11cad4db889264242bdb367
John Gosset <john@gossetx.com>
2019-07-12 15:07:37 -0400

    Add git aliases

--- home.nix
+++ home.nix
@@ -218,6 +218,28 @@
         "*.pyc"
         "*.iso"
       ];
+
+      aliases = {
+        br = "branch -avv";
+        ci = "commit";
+        co = "checkout";
+        de = "daemon --verbose --export-all";
+        dl = "diff HEAD^ HEAD";
+        ds = "diff --staged";
+        lasttag = "describe --tags --abbrev=0";
+        lg = "log --pretty=format:'%C(yellow)%h%Creset %s  %C(red)<%cn> %Cgreen[%cr] %Creset%d' --graph";
+        lga = "log --pretty=format:'%C(yellow)%h%Creset %s  %C(red)<%cn> %Cgreen[%cr] %Creset%d' --graph --all";
+        re = "remote -v";
+        reu = "remote set-url";
+        st = "status --column";
+
+        # Via https://git.wiki.kernel.org/index.php/Aliases#Use_graphviz_for_display
+        graphviz = "!f() { echo 'digraph git {' ; git log --pretty='format:  %h -> { %p }' \"$@\" | sed 's/[0-9a-f][0-9a-f]*/\"&\"/g' ; echo '}'; }; f";
+
+	# find fat git files
+	# via: https://stackoverflow.com/questions/9456550/how-to-find-the-n-largest-files-in-a-git-repository#comment59168142_28783843
+	fatfiles = "!f() { git ls-tree -r -l --abbrev --full-name HEAD | sort -rnk4 | head -20; }; f";
+      };
     };
 
     go = {
