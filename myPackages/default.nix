3523aa50be85e01f4af9f3ec4b842262f92b946d
John Gosset <john@gossetx.com>
2019-07-17 14:09:50 -0400

    Add FIXME re: loccount not compiling

--- myPackages/default.nix
+++ myPackages/default.nix
@@ -8,5 +8,5 @@ with pkgs;
   go-4d = callPackage ./4d {};
   horeb = callPackage ./horeb {};
   mtlcam = callPackage ./mtlcam {};
-  loccount = callPackage ./loccount {};
+  loccount = callPackage ./loccount {};  # FIXME: loccount doesn't compile.
 }
