e79cc4c571488b8c276e2080f4d04897a613453d
John Gosset <john@gossetx.com>
2019-07-24 08:48:57 -0400

    tmux: add new-session & reload conf keybindings

--- home.nix
+++ home.nix
@@ -188,6 +188,9 @@ in
         bind-key '"' split-window -c "#{pane_current_path}"
         bind-key % split-window -h -c "#{pane_current_path}"
         bind-key c new-window -c "#{pane_current_path}"
+
+        bind-key S command-prompt -p 'new-session name:' "new-session -s %1"
+        bind-key R source-file ~/.tmux.conf; display-message "source-file ~/.tmux.conf"
       '';
     };
 
