3fecf85b862c49d3c9d0f8f8c94a11e2d9dc39c6
John Gosset <john@gossetx.com>
2019-07-28 22:21:59 -0400

    Update comment

--- home.nix
+++ home.nix
@@ -1,9 +1,11 @@
 # home-manager configuration.
 # See https://github.com/rycee/home-manager
 
-# To choose, run (for example):
+# Below are machine-specific configurations.
+# To choose a machine, run (for example):
 #   home-manager -A luban switch
 # Ref: home-manager(1)
+
 {
   luban   = import ./machines/luban;
   eiffel  = import ./machines/eiffel;
