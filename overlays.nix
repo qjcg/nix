1aca917f3215834250a5ca193f369b31b7f179a5
John Gosset <john@gossetx.com>
2019-08-03 20:29:16 -0400

    Update st overlay

--- overlays.nix
+++ overlays.nix
@@ -8,9 +8,14 @@
     horeb = super.callPackage ./packages/horeb {};
 
     # Overriding existing packages.
+    # Refs:
+    #   - https://nixos.org/nixpkgs/manual/#sec-pkg-override
+    #   - https://nixos.org/nixpkgs/manual/#sec-pkg-overrideAttrs
     st = super.st.override {
       conf = builtins.readFile ./files/st-config.h;
-    };
+    } // super.st.overrideAttrs (oldAttrs: {
+      meta.priority = "4";
+    });
 
   }
 
