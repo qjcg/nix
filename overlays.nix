d66ade68ef6e15c65928f98f00491a500ec1d459
John Gosset <john@gossetx.com>
2019-08-04 13:31:43 -0400

    Specify rec when overriding attributes

--- overlays.nix
+++ overlays.nix
@@ -13,7 +13,7 @@
     #   - https://nixos.org/nixpkgs/manual/#sec-pkg-overrideAttrs
     st = super.st.override {
       conf = builtins.readFile ./files/st-config.h;
-    } // super.st.overrideAttrs (oldAttrs: {
+    } // super.st.overrideAttrs (oldAttrs: rec {
       meta.priority = "4";
     });
 
