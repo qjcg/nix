5dfa424500c4ad92c9c687f5d3b49f2c99538f1d
John Gosset <john@gossetx.com>
2019-08-05 16:22:08 -0400

    Add s-nail config file

---
+++ files/mailrc.nix
@@ -0,0 +1,16 @@
+{ secrets, ... }:
+
+''
+set v15-compat
+set sendcharsets=utf-8
+set sendwait
+set noheader
+set noasksend
+
+define InboxHook {
+	search :fu
+}
+set folder-hook-+INBOX=InboxHook
+
+${secrets.s-nail-accounts}
+''
