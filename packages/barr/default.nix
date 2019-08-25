038c882ed7ec2c0f32cc61ee54fa8d24ebd4fc5f
John Gosset <john@gossetx.com>
2019-08-25 17:58:15 -0400

    Bump barr version

--- packages/barr/default.nix
+++ packages/barr/default.nix
@@ -7,7 +7,7 @@
 
 buildGoModule rec {
   name = "barr-${version}";
-  version = "1.13.0";
+  version = "1.13.1";
 
   subPackages = [ "cmd/barr" ];
 
@@ -18,13 +18,13 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "19fl8wrhclayfj3f97zdnwqy6b1xj12x08x51xz90rf2j5f4lvr9";
+    sha256 = "1ijcrzsql9b81z9brqzizl7gf41rn18fnb2xrvhxrdhhmavh6f5i" ;
   };
 
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  modSha256 = "0l7vj9b7j3mp0llhhhs47jmfzr5i343kkxc1n7blhmbddgdqrgpx";
+  modSha256 = "0l7vj9b7j3mp0llhhhs47jmfzr5i343kkxc1n7blhmbddgdqrgpx" ;
 
   meta = with lib; {
     description = "A simple statusbar.";
