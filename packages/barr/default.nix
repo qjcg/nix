39d1a5fa3c447e852300de59ff5d7f620034bc18
John Gosset <john@gossetx.com>
2019-08-25 19:09:45 -0400

    Bump barr version

--- packages/barr/default.nix
+++ packages/barr/default.nix
@@ -7,7 +7,7 @@
 
 buildGoModule rec {
   name = "barr-${version}";
-  version = "1.13.1";
+  version = "1.13.2";
 
   subPackages = [ "cmd/barr" ];
 
@@ -18,7 +18,7 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "1ijcrzsql9b81z9brqzizl7gf41rn18fnb2xrvhxrdhhmavh6f5i" ;
+    sha256 = "0d6alra0j4z7i4hca0pqdknrf8gxwynfisr0vwd2z804qidyx20k" ;
   };
 
   # First, provide a fake hash via the value: lib.fakeSha256
