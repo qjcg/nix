6909efccffbc2bb49cd0f6303a154a331d36a2a8
John Gosset <john@gossetx.com>
2019-09-03 09:23:22 -0400

    Add retroarch override TODO

--- overlays.nix
+++ overlays.nix
@@ -26,6 +26,19 @@
       dunstify = true;
     };
 
+    # TODO: Figure out syntax for enabling various cores (below does NOT work).
+    #retroarch = super.retroarch.override {
+    #  cores = [
+    #    fba
+    #    genesis-plus-gx
+    #    mame
+    #    mupen64plus
+    #    nestopia
+    #    snes9x-next
+    #    stella
+    #  ];
+    #};
+
     st = super.st.override {
       conf = builtins.readFile ./files/st-config.h;
     };
