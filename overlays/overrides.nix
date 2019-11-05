5bebbf4591dc911f0f604bf16c9b5016db84e096
John Gosset <john@gossetx.com>
2019-11-05 17:35:27 -0500

    Bump delve to v1.3.2 via overlay / overrideAttrs

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -1,6 +1,16 @@
 self: super:
 
 {
+  delve = super.delve.overrideAttrs (oldAttrs: rec {
+    version = "1.3.2";
+    src = self.fetchFromGitHub {
+      owner = "go-delve";
+      repo = "delve";
+      rev = "v${version}";
+      sha256 = "0i2sxr5d4ylakn93lmp65xwhaalbv01q4dih6viwnb9d0gq7p26x";
+    };
+  });
+
   dunst = super.dunst.override {
     dunstify = true;
   };
