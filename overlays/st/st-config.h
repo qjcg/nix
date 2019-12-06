6cd44c6a33660c52d31569962fb959a3acb255c6
John Gosset <john@gossetx.com>
2019-12-06 09:55:56 -0500

    Specify st-font as monospace and use fontconfig to choose

--- overlays/st/st-config.h
+++ overlays/st/st-config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka-11:weight=regular:antialias=true:autohint=true";
+static char *font = "monospace-11:weight=regular:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
