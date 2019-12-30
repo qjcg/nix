8d7e460f54d3e1ebdfb84f19f473ee18a89e264f
John Gosset <john@gossetx.com>
2019-12-30 14:43:41 -0500

    Add hydra & dumb-jump

--- files/init.el
+++ files/init.el
@@ -156,6 +156,26 @@
 (use-package all-the-icons
   :ensure t)
 
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g b" . dumb-jump-back)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+  :ensure t)
+
+(defhydra dumb-jump-hydra (:color blue :columns 3)
+    "Dumb Jump"
+    ("j" dumb-jump-go "Go")
+    ("o" dumb-jump-go-other-window "Other window")
+    ("e" dumb-jump-go-prefer-external "Go external")
+    ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+    ("i" dumb-jump-go-prompt "Prompt")
+    ("l" dumb-jump-quick-look "Quick look")
+    ("b" dumb-jump-back "Back"))
+
 (use-package discover-my-major
   :ensure t)
 
@@ -166,6 +186,9 @@
   :ensure t)
 
 (use-package gitignore-mode
+  :ensure t)
+
+(use-package hydra
   :ensure t)
 
 (use-package jinja2-mode
