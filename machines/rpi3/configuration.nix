0944e1ab5f5ce249498a6f0a89b238f8445373ce
John Gosset <john@gossetx.com>
2020-01-19 13:35:20 -0500

    Simplify rpi3 configuration.nix

--- machines/rpi3/configuration.nix
+++ machines/rpi3/configuration.nix
@@ -1,29 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  hardware.enableRedistributableFirmware = true;
-
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 
-  # This stanza is NEEDED for rpi3b+ wifi card to work
-  # (taken from Nix wiki & tested 2019-04-26, kernel 5.0.9).
-  #hardware.firmware = [
-  #  (pkgs.stdenv.mkDerivation {
-  #     name = "broadcom-rpi3bplus-extra";
-  #     src = pkgs.fetchurl {
-  #       url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/b518de4/brcm/brcmfmac43455-sdio.txt";
-  #       sha256 = "0r4bvwkm3fx60bbpwd83zbjganjnffiq1jkaj0h20bwdj9ysawg9";
-  #     };
-  #     phases = [ "installPhase" ];
-  #     installPhase = ''
-  #       mkdir -p $out/lib/firmware/brcm
-  #       cp $src $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
-  #     '';
-  #  })
-  #];
-
-  # File systems configuration for using the installer's partition layout
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
@@ -33,8 +13,7 @@
 
   swapDevices = [ { device = "/swapfile"; size = 4096 ; } ];
 
-  boot.loader.grub.enable = false;
-  boot.cleanTmpDir = true;
+  hardware.enableRedistributableFirmware = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # A bunch of boot parameters needed for optimal runtime on RPi 3b+
@@ -46,6 +25,7 @@
   boot.loader.raspberryPi.firmwareConfig = ''
     gpu_mem=256
   '';
+  boot.loader.grub.enable = false;
 
   networking.wireless.iwd.enable = true;
 
@@ -82,18 +62,14 @@
 
   time.timeZone = "America/Montreal";
 
-  users.extraUsers = {
+  users.users = {
     john = {
       isNormalUser = true; 
       uid = 7777;
       extraGroups = [
       	"wheel"
       ];
-
-      # FIXME: Use mkpasswd to generate a password
-      #hashedPassword = "";
     };
   };
 
 }
-
