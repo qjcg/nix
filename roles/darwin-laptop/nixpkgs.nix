b4f5d453870089d49ff16ec224bbaf597e377d82
John Gosset <john@gossetx.com>
2020-01-19 21:14:38 -0500

    Update darwin-laptop import paths

--- roles/darwin-laptop/nixpkgs.nix
+++ roles/darwin-laptop/nixpkgs.nix
@@ -1,15 +1,10 @@
 {
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnsupportedSystem = true;
+
   nixpkgs.overlays = [
-    (import ../../overlays/4d)
-    (import ../../overlays/glooctl)
-    (import ../../overlays/hey)
-    (import ../../overlays/horeb)
-    (import ../../overlays/k3d)
-    (import ../../overlays/mtlcam)
-    (import ../../overlays/neovim)
-    (import ../../overlays/skaffold)
-    (import ../../overlays/overrides.nix)
+    (import ../../packages)
+    (import ../../packages/neovim)
+    (import ../../packages/overrides.nix)
   ];
 }
