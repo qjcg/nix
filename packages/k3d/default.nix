0a6f48e16232a700a23aa938d4512542745673ef
John Gosset <john@gossetx.com>
2020-01-27 07:52:45 -0500

    Bump k3d package -> 1.5.1

--- packages/k3d/default.nix
+++ packages/k3d/default.nix
@@ -7,7 +7,7 @@
 
 buildGoModule rec {
   name = "k3d-${version}";
-  version = "1.4.0";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "rancher";
@@ -16,14 +16,14 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "0dbyyzyprhxsq6ardq380hppzdg01crppad5fczzlqsaxxvac039";
+    sha256 = "0qc6mhx7mhlqixl4qq4yz69af9qsyq22454v3mh8cpqf627y468y";
   };
 
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
   #modSha256 = lib.fakeSha256;
-  modSha256 = "1xp5q21nsc920hqy28k514g4s3mwp5462lmdl6b60lfhlaf23940";
+  modSha256 = "0c8bfl0hz5cfhi6jzhhylz051jiix6s7s20fn23w7wri4xaqrjn8";
 
   meta = with lib; {
     description = "Little helper to run Rancher Lab's k3s in Docker";
