f29e85f7f89dfbe568f6a54ae31ea1fd9c39e484
John Gosset <john@gossetx.com>
2020-02-19 07:19:03 -0500

    Update configuration-example.nix

--- configuration-example.nix
+++ configuration-example.nix
@@ -1,10 +1,32 @@
 { config, pkgs, ... }:
 
+let
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (import ./packages)
+
+      # TODO: Clearly distinguish/separate overlays (below) from packages.
+      (import ./packages/neovim)
+      (import ./packages/st)
+      (import ./packages/sxiv)
+      (import ./packages/vscodium-with-extensions)
+
+      (import ./packages/overrides.nix)
+      (import ./packages/environments.nix)
+    ];
+  };
+
+  secrets = import ./secrets.nix ;
+in
 {
-  # Replace the imports below with those for the local configuration.
+
   imports = [
     ./machines/luban
     ./roles/gnome-workstation
-    ./users/john.nix
+    ./roles/wayfire-workstation
+    (import ./users/john.nix { inherit config pkgs secrets; })
   ];
+
+  nixpkgs.config.allowUnfree = true;
+
 }
