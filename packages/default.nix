ae4ab79686c3a7daf32073a7bcfca6d94427c686
John Gosset <john@gossetx.com>
2020-02-19 19:35:55 -0500

    Tweak packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,28 +1,25 @@
 self: super:
 
 {
-    # Packages.
-    go-4d = super.callPackage ./4d {};
-    barr = super.callPackage ./barr {};
-    battery = super.callPackage ./battery {};
-    brightness = super.callPackage ./brightness {};
-    cassowary = super.callPackage ./cassowary {};
-    emacs = super.callPackage ./emacs {};
-    emacs-nox = super.callPackage ./emacs-nox {};
-    freetube = super.callPackage ./freetube {};
-    glooctl = super.callPackage ./glooctl {};
-    goplot = super.callPackage ./goplot {};
-    hey = super.callPackage ./hey {};
-    horeb = super.callPackage ./horeb {};
-    k3c = super.callPackage ./k3c {};
-    k3d = super.callPackage ./k3d {};
-    kompose = super.callPackage ./kompose {};
-    loccount = super.callPackage ./loccount {};
-    ludo = super.callPackage ./ludo {};
-    mtlcam = super.callPackage ./mtlcam {};
-
-    #neovim = super.callPackage ./neovim {inherit self super; };
-
-    skaffold = super.callPackage ./skaffold {};
-    s-nail = super.callPackage ./s-nail {};
+  # Packages.
+  go-4d = super.callPackage ./4d {};
+  barr = super.callPackage ./barr {};
+  battery = super.callPackage ./battery {};
+  brightness = super.callPackage ./brightness {};
+  cassowary = super.callPackage ./cassowary {};
+  emacs = super.callPackage ./emacs {};
+  emacs-nox = super.callPackage ./emacs-nox {};
+  freetube = super.callPackage ./freetube {};
+  glooctl = super.callPackage ./glooctl {};
+  goplot = super.callPackage ./goplot {};
+  hey = super.callPackage ./hey {};
+  horeb = super.callPackage ./horeb {};
+  k3c = super.callPackage ./k3c {};
+  k3d = super.callPackage ./k3d {};
+  kompose = super.callPackage ./kompose {};
+  loccount = super.callPackage ./loccount {};
+  ludo = super.callPackage ./ludo {};
+  mtlcam = super.callPackage ./mtlcam {};
+  skaffold = super.callPackage ./skaffold {};
+  s-nail = super.callPackage ./s-nail {};
 }
