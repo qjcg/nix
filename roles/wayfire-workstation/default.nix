c67d75cddb202bea9f8486fc4a8a4e9beb128575
John Gosset <john@gossetx.com>
2020-05-04 22:06:29 -0400

    Re-enable i3status-rust

--- roles/wayfire-workstation/default.nix
+++ roles/wayfire-workstation/default.nix
@@ -9,7 +9,7 @@
     swaylock # used for locking Wayland sessions
 
     waybar         # polybar-alike
-    #i3status-rust # simpler bar written in Rust
+    i3status-rust  # simpler bar written in Rust
 
     gebaar-libinput  # libinput gestures utility
     glpaper          # GL shaders as wallpaper
