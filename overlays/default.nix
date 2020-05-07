e7579a0507e2309ed88de6f12d3e2953091d72be
John Gosset <john@gossetx.com>
2020-05-07 11:32:56 -0400

    Add vscodium extensions

--- overlays/default.nix
+++ overlays/default.nix
@@ -75,11 +75,22 @@ self: super:
       ms-kubernetes-tools.vscode-kubernetes-tools
       ms-azuretools.vscode-docker
       #ms-python.python
-      ms-vscode.Go
       ms-vscode-remote.remote-ssh
       redhat.vscode-yaml
       #vscodevim.vim
     ]) ++ super.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "Go";
+        publisher = "ms-vscode";
+        version = "0.14.1";
+        sha256 = "1zj87m0swjnr7k9nwxj2db96h90fs1cny7w9c0q4kqnrrysh8ala";
+      }
+      {
+        name = "trailing-spaces";
+        publisher = "shardulm94";
+        version = "0.3.1";
+        sha256 = "0h30zmg5rq7cv7kjdr5yzqkkc1bs20d72yz9rjqag32gwf46s8b8";
+      }
       {
         name = "remote-containers";
         publisher = "ms-vscode-remote";
@@ -87,11 +98,33 @@ self: super:
         sha256 = "1x7fwcajsgp790b5z9f24a7dk169b5b5wn75cybd5w8kk9w8qvxk";
       }
       {
+        name = "dotenv";
+        publisher = "mikestead";
+        version = "1.0.1";
+        sha256 = "0rs57csczwx6wrs99c442qpf6vllv2fby37f3a9rhwc8sg6849vn";
+      }
+
+      # THEMES
+
+      {
+        name = "horizon-vscode";
+        publisher = "bauke";
+        version = "3.0.2";
+        sha256 = "1arrn6y485hz0igzi9hsjn5w1wgq0i85xsadc8fypp646qgqijqz";
+      }
+      {
+        name = "material-icon-theme";
+        publisher = "pkief";
+        version = "4.1.0";
+        sha256 = "00alw214i2iibaqrm1njvb13bb41z3rvgy1akyq8fxvz35vq5a2s";
+      }
+      {
         name = "winteriscoming";
         publisher = "johnpapa";
         version = "1.4.2";
         sha256 = "0rqmzr6bbaga45idnxbwggb9w578ky4ljx9slvkbc6yibqfskvpl";
       }
+
     ];
   };
 
