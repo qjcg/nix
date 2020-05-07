2154d9dd6f02ea6b16b9a194bf68e784c6b168fe
John Gosset <john@gossetx.com>
2020-05-06 20:56:26 -0400

    Update jmigpin-editor

--- packages/jmigpin-editor/default.nix
+++ packages/jmigpin-editor/default.nix
@@ -7,7 +7,7 @@
 
 buildGoModule rec {
   name = "jmigpin-editor-${version}";
-  version = "1.1.1";
+  version = "2.0.7";
 
   src = fetchFromGitHub {
     owner = "jmigpin";
@@ -16,13 +16,16 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "155cr42pzxwjy756hrys8wbmghd690fn2m1gfys8wxfca4knjlq4";
+    sha256 = "1xky3m69j1m0f8k4ar8v30k4sc51gwsnrhixx3f7zax5wz28jclm";
   };
+
+  subPackages = [ "." ];
 
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  modSha256 = "00xwzi31cg114p18fpdl13dx1z546v0566hp6mjsfcm9j2mynhax";
+  modSha256 = "1vg1x2jbdg8b8qi3rnyxsb3cqp02qzh5y9lz4ml7bln47808wd0f";
+  modRoot = "v2";
 
   meta = with lib; {
     description = "Source code editor in pure Go.";
