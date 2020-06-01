a2fa4626d256478981abe006c68a08b44ec13b6d
John Gosset <john@gossetx.com>
2020-06-01 11:42:10 -0400

    Use barr as sway statusbar

--- packages/barr/default.nix
+++ packages/barr/default.nix
@@ -7,7 +7,7 @@
 
 buildGoModule rec {
   name = "barr-${version}";
-  version = "1.13.3";
+  version = "1.14.2-alpha";
 
   src = fetchFromGitHub {
     owner = "qjcg";
@@ -16,7 +16,7 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "1r4sf3wrhdvbxv8wp9597hxindsqzffzkpqs6vb5b6x4x4mlhdxg" ;
+    sha256 = "0qp4ci0rjlchpiq7200hqcyar89cgw7i4i063v7lzvq8gw2p1v1l" ;
   };
 
   subPackages = [ "cmd/barr" ];
@@ -24,7 +24,7 @@ buildGoModule rec {
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  modSha256 = "1n9rcjbmfcw4n8bdr93wvh8cgbqsyxr0w902k638ykz9lgx94kvp" ;
+  modSha256 = "085m3ypkkyav143mhw31fyvbwn8x00f27ypzmdg1k1qjggh40zy6" ;
 
   meta = with lib; {
     description = "A simple statusbar.";
