03caa8dbf8dc15dc8908cfd90c044c3efe23b62d
John Gosset <john@gossetx.com>
2020-06-01 18:08:10 -0400

    Bump barr version

--- packages/barr/default.nix
+++ packages/barr/default.nix
@@ -7,7 +7,7 @@
 
 buildGoModule rec {
   name = "barr-${version}";
-  version = "1.14.13-alpha";
+  version = "1.14.14-alpha";
 
   src = fetchFromGitHub {
     owner = "qjcg";
@@ -16,7 +16,7 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "0l26r9wlbz7kbwjxi01nc5s2fb9062dhbs7fmlzggd7am9ibcgiz" ;
+    sha256 = "1wxpkcqmz810arw3fv9q3jvv5w41bv6mcb4fbzhw9b4sm1j69adc" ;
   };
 
   subPackages = [ "cmd/barr" ];
