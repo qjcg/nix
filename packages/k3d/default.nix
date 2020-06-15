29015b6e233f0ad03f3899a9f800944903be5383
John Gosset <john@gossetx.com>
2020-06-15 10:45:30 -0400

    Add build-time version setting to k3d package

--- packages/k3d/default.nix
+++ packages/k3d/default.nix
@@ -1,14 +1,7 @@
-{
-  pkgs,
-  lib,
-  fetchFromGitHub,
-  buildGoModule,
-  stdenv,
-  darwin,
-}:
+{ pkgs, lib, fetchFromGitHub, buildGoModule }:
 
 buildGoModule rec {
-  name = "k3d-${version}";
+  pname = "k3d";
   version = "1.7.0";
 
   src = fetchFromGitHub {
@@ -21,21 +14,13 @@ buildGoModule rec {
     sha256 = "0aij2l7zmg4cxbw7pwf7ddc64di25hpjvbmp1madhz9q28rwfa9w";
   };
 
-  # Ref: https://github.com/tsaeger/nixpkgs/commit/39386e9f81552720468acd0eb35becec863c0e37
-  buildInputs = []
-  ++ stdenv.lib.optionals stdenv.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-  ];
+  buildFlagsArray = [ "-ldflags=-s -w -X github.com/rancher/k3d/version.Version=${version}" ];
 
-  # First, provide a fake hash via the value: lib.fakeSha256
-  # Then, during build, copy "got" value in here.
-  # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  #vendorSha256 = lib.fakeSha256;
-  vendorSha256 = "0c8bfl0hz5cfhi6jzhhylz051jiix6s7s20fn23w7wri4xaqrjn8";
+  deleteVendor = true;
 
   meta = with lib; {
-    description = "Little helper to run Rancher Lab's k3s in Docker";
-    homepage = https://github.com/rancher/k3d ;
+    description = "A lightweight wrapper to run k3s (Rancher Lab’s minimal Kubernetes distribution) in docker";
+    homepage = "https://github.com/rancher/k3d" ;
     license = licenses.mit;
   };
 }
