3c04531f1090e564028a9b9e4f7cc6115fbbe51e
John Gosset <john@gossetx.com>
2020-06-22 14:15:34 -0400

    Bump skaffold version to v1.11.0

--- packages/skaffold/default.nix
+++ packages/skaffold/default.nix
@@ -1,28 +1,25 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "skaffold";
-  version = "1.1.0";
-  # rev is the 1.1.0 commit, mainly for skaffold version command output
-  rev = "2f14d99fc5f81e3a52dd76d43cad8d014f150327";
-
-  goPackagePath = "github.com/GoogleContainerTools/skaffold";
-  subPackages = [ "cmd/skaffold" ];
-
-  buildFlagsArray = let t = "${goPackagePath}/pkg/skaffold";
-  in ''
-    -ldflags=
-      -X ${t}/version.version=v${version}
-      -X ${t}/version.gitCommit=${rev}
-      -X ${t}/version.buildDate=unknown
-  '';
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "GoogleContainerTools";
     repo = "skaffold";
     rev = "v${version}";
-    sha256 = "1a4gkr0g620d4hxfxzk2aasic91assgwf0flms8av2nm2pkcv7yw";
+    sha256 = "035xp34m8kzb75mivgf3kw026n2h6g2a7j2mi32nxl1a794w36zi";
   };
+
+  deleteVendor = true;
+  vendorSha256 = "1s5747b1zcl1g4zxbqya3ma1a52kkiw33j9h73r05r9kgashx2l4";
+  subPackages = [ "cmd/skaffold" ];
+
+  buildFlagsArray =
+    let t = "github.com/GoogleContainerTools/skaffold/pkg/skaffold/version";
+    in ''
+      -ldflags= -s -w -X ${t}.version=v${version}
+    '';
 
   meta = with stdenv.lib; {
     description = "Easy and Repeatable Kubernetes Development";
