07b8a3417028842a17e8cb8a4ca9a6b25ce2302b
John Gosset <john@gossetx.com>
2020-06-26 09:08:59 -0400

    Update k3c package

--- packages/k3c/default.nix
+++ packages/k3c/default.nix
@@ -2,28 +2,30 @@
 
 buildGoModule rec {
   pname = "k3c";
-  version = "0.0.1";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
-    owner = "ibuildthecloud";
+    owner = "rancher";
     repo = "k3c";
     rev = "v${version}";
-
-    # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
-    #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "0z22vixg14bqs04dfb7qhjlc94v8x97hv9kgqzab7lz95xcf0mqb";
+    sha256 = "1ni7pab7hk9rkd7h4q7a6hcjdrsayqxfyj1jzvya0qra294flkkd";
   };
+
+  subPackages = [ "." ];
+  buildFlagsArray = let pkg = "github.com/rancher/k3c";
+  in [ "-ldflags=-s -w -X ${pkg}/pkg/version.Version=v${version}" ];
 
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
   #vendorSha256 = lib.fakeSha256;
-  vendorSha256 = "0wgx3jlw92bzwcbwqbq3hrv4q1z2mzrvxnld6bj5rgkwym1f8zvs";
+  deleteVendor = true;
+  vendorSha256 = "0iiznjcx8r0v7mjbvry00cf4d4b52ngy036xm8fl3yx1kj0cqwfy";
 
   meta = with stdenv.lib; {
     description =
       "Lightweight local container engine for container development";
-    homepage = "https://github.com/ibuildthecloud/k3c";
+    homepage = "https://github.com/rancher/k3c";
     license = licenses.asl20;
   };
 }
