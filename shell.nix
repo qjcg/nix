7a6bbb79db38b1e03e7a80a951d337ebf3103a17
John Gosset <john@gossetx.com>
2020-06-26 09:13:27 -0400

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,3 +1,22 @@
 with import <nixpkgs> { };
 
-mkShell { buildInputs = [ fd fzf neovim nixfmt ]; }
+mkShell {
+  pname = "shell-nixpkgs";
+
+  buildInputs = [ fd fzf neovim nixfmt ];
+
+  shellHook = let
+    base_url = "https://api.github.com";
+    repos =
+      toString [ "rancher/k3c" "rancher/k3d" "bitnami-labs/sealed-secrets" ];
+  in ''
+    alias ls='ls --color --group-directories-first'
+
+    releases() {
+      for r in ${repos}; do
+        url=${base_url}/repos/$r/releases/latest
+        curl -s $url | jq '{tag_name, name, published_at}'
+      done
+    }
+  '';
+}
