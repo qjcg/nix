0b2040874c88d521558fdc6460dffc8a77307949
John Gosset <john@gossetx.com>
2020-06-29 10:19:47 -0400

    Bump benthos package

--- packages/benthos/default.nix
+++ packages/benthos/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "benthos";
-  version = "3.18.0";
+  version = "3.19.0";
 
   src = fetchFromGitHub {
     owner = "Jeffail";
@@ -11,7 +11,7 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "1nh1n5h1mvzpbvqgza45pgzh2rc96g7jj75scglzvzxq62syv9jl";
+    sha256 = "0qjlm4r3hh2268w5g4b8z1158wpbiffrwqw2ifs6ib8bwjknd85q";
   };
 
   buildFlagsArray = [
@@ -23,7 +23,7 @@ buildGoModule rec {
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  vendorSha256 = "1s0p3glszr81wxk0h38n55zcpzchacrnwr725jk4jqdpfifrn1qk";
+  vendorSha256 = "0ad0h6lraz0b51zm8jc079zb34rmggv1qxra1rh58pbvfsfl11g7";
 
   meta = with stdenv.lib; {
     description = "A stream processor for mundane tasks written in Go";
