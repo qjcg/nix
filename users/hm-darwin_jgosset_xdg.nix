d10fcbc5150701f155897a796e32ffa2812ca912
John Gosset <john@gossetx.com>
2020-07-07 12:16:47 -0400

    Remove cruft

--- users/hm-darwin_jgosset_xdg.nix
+++
@@ -1,8 +0,0 @@
-{
-  xdg.configFile = {
-    "cmus/rc".source = ../files/cmusrc;
-    "emacs/init.el".source = ../files/emacs/init.el;
-    "nvim/coc-settings-example.json".source = ../files/coc-settings.json;
-    "xonsh/".source = ../files/xonsh;
-  };
-}
