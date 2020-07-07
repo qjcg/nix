2960fd2c7f69bf55f06df558e8a64c80cdef25f2
John Gosset <john@gossetx.com>
2020-07-07 18:09:42 -0400

    Simplify configuration-example.nix

--- configuration-example.nix
+++ configuration-example.nix
@@ -3,36 +3,35 @@
 { config, pkgs, ... }:
 
 let
-  overlay-mine = (import ./overlays);
+  secrets = import ./secrets.nix;
+  overlay-mine = import ./overlays;
 
   # https://github.com/nix-community/emacs-overlay/commits/master
-  overlay-emacs = (import (fetchGit {
+  overlay-emacs = import (fetchGit {
     url = "https://github.com/nix-community/emacs-overlay";
     ref = "master";
     rev = "7a9694ef831a848fd423bbbd60e807b236647bb8";
-  }));
+  });
 
   # https://github.com/nix-community/emacs-overlay/commits/master
-  overlay-wayland = (import (fetchGit {
+  overlay-wayland = import (fetchGit {
     url = "https://github.com/colemickens/nixpkgs-wayland";
     ref = "master";
     rev = "724ac75779a1d411e89ebe0ab4aab721e0af525b";
-  }));
+  });
 
-  pkgs = (import (fetchGit {
+  pkgs = import (fetchGit {
     url = "https://github.com/NixOS/nixpkgs-channels";
     ref = "nixpkgs-20.03-darwin";
 
     # If `rev` is assigned a value, the channel will be pinned to this revision.
     # If not, the latest commit relative to the `ref` above will be used.
     rev = "02203c195495aeb5fa1eeffea6cfa8a291de05a8";
-  }) { overlays = [ overlay-emacs overlay-wayland overlay-mine ]; });
-
-  secrets = (import ./secrets.nix);
+  }) { overlays = [ overlay-emacs overlay-wayland overlay-mine ]; };
 in {
   imports = [
     (import ./roles/darwin-laptop { inherit pkgs; })
-    (import ./users/joe.nix { inherit pkgs secrets; })
+    (import ./users/hm-darwin_jgosset.nix { inherit pkgs secrets; })
   ];
 
   inherit (secrets) users;
