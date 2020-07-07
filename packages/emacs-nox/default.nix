7a6cfb6432eb97caef41638d7bbbf03349666ee9
John Gosset <john@gossetx.com>
2020-07-07 17:49:27 -0400

    Use pinned emacs packages

--- packages/emacs-nox/default.nix
+++ packages/emacs-nox/default.nix
@@ -2,20 +2,9 @@
 
 { pkgs, }:
 
-let
-  # From: https://github.com/Jeschli/jeschli-nix/blob/3daaafeca3531e5390812e406886892e4371890f/emacs.nix
-  pkgsWithOverlay = import <nixpkgs> {
-    overlays = [
-      (import (builtins.fetchTarball {
-        url =
-          "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      }))
-    ];
-  };
-  myConfig = ../../files/emacs/init.el;
-in pkgsWithOverlay.emacsWithPackagesFromUsePackage {
-  config = builtins.readFile myConfig;
-  package = pkgsWithOverlay.emacs-nox;
+pkgs.emacsWithPackagesFromUsePackage {
+  config = builtins.readFile ../../files/emacs/init.el;
+  package = pkgs.emacs-nox;
 
   # TODO: Figure out how to deploy my personal init.el via this overlay (i.e. avoiding home-manager).
   #override = epkgs: epkgs // {
@@ -29,4 +18,3 @@ in pkgsWithOverlay.emacsWithPackagesFromUsePackage {
   #  myConfig
   #];
 }
-
--- packages/emacs/default.nix
+++ packages/emacs/default.nix
@@ -2,19 +2,8 @@
 
 { pkgs, }:
 
-let
-  # From: https://github.com/Jeschli/jeschli-nix/blob/3daaafeca3531e5390812e406886892e4371890f/emacs.nix
-  pkgsWithOverlay = import <nixpkgs> {
-    overlays = [
-      (import (builtins.fetchTarball {
-        url =
-          "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      }))
-    ];
-  };
-  myConfig = ../../files/emacs/init.el;
-in pkgsWithOverlay.emacsWithPackagesFromUsePackage {
-  config = builtins.readFile myConfig;
+pkgs.emacsWithPackagesFromUsePackage {
+  config = builtins.readFile ../../files/emacs/init.el;
 
   # TODO: Figure out how to deploy my personal init.el via this overlay (i.e. avoiding home-manager).
   #override = epkgs: epkgs // {
