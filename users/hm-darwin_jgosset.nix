fbd4e75f6062d92335cb264a996c2763e901a27c
John Gosset <john@gossetx.com>
2020-07-14 09:28:55 -0400

    Add alacritty and bash configs for jgosset user

--- users/hm-darwin_jgosset.nix
+++ users/hm-darwin_jgosset.nix
@@ -11,6 +11,70 @@
 
   home-manager.users.jgosset = {
     programs = {
+
+      alacritty = {
+        enable = true;
+        settings = {
+          font.normal.family = "Fira Code";
+          font.normal.style = "Retina";
+          font.size = 16.0;
+
+          colors.cursor.text = "#000000";
+          colors.cursor.cursor = "#ef02f7";
+          draw_bold_text_with_bright_colors = true;
+        };
+      };
+
+      bash = {
+        enable = true;
+        profileExtra = ''
+          # nodejs
+          export npm_config_prefix=~/.node_modules
+          export PATH=$npm_config_prefix/bin:$PATH
+
+          # go
+          export GOPATH=~/go
+          export GOBIN=$GOPATH/bin
+          export PATH=$GOBIN:$PATH
+
+          # nix
+          #if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+        '';
+
+        initExtra = ''
+          command -v direnv >/dev/null && eval "$(direnv hook bash)"
+        '';
+
+        sessionVariables = rec {
+          BROWSER = "firefox";
+          EDITOR = "nvim";
+          LC_COLLATE = "C";
+          PAGER = "less";
+          PS1 = "\\u@\\h \\W \\$ ";
+
+          FZF_DEFAULT_COMMAND = "fd --type f --color=never";
+          FZF_CTRL_T_COMMAND = "${FZF_DEFAULT_COMMAND}";
+          FZF_ALT_C_COMMAND = "fd --type d . --color=never";
+          FZF_DEFAULT_OPTS =
+            " --height 75% --multi --reverse --bind ctrl-f:page-down,ctrl-b:page-up ";
+        };
+
+        shellAliases = rec {
+          # Print all Active Directory groups. It seems they have GID >= 10000.
+          drwADGroups =
+            "id | sed -e 's/,/\\n/g' -e 's/(/: /g' -e 's/)//g' | sort -n | awk -F: '/^[1-9]/ && $1 > 10000'";
+          drwHomeUsage =
+            "shopt -s dotglob && du --threshold 1M --exclude={G,H,W,X} -s ~/* | sort -n | sed 's/.home.jgosset.//' | awk '{print $2,$1}' | goplot bar";
+          grep = "grep -E";
+          k = "kubectl";
+          ls =
+            "${pkgs.coreutils}/bin/ls --color --human-readable --group-directories-first";
+          tree = "tree -A -C";
+          vi = "nvim";
+          vim = "nvim";
+        };
+      };
+
       git = {
         enable = true;
         userName = "${secrets.git-name}";
