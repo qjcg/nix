5d486f7f9dfd5c85eb92504bd3875c627530cc70
John Gosset <jgosset@drw.com>
2020-07-27 18:32:51 -0400

    Bump benthos version

--- packages/benthos/default.nix
+++ packages/benthos/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "benthos";
-  version = "3.21.0";
+  version = "3.23.0";
 
   src = fetchFromGitHub {
     owner = "Jeffail";
     repo = "benthos";
     rev = "v${version}";
-    sha256 = "1vdv5yawb68qbfddgd8502sc3aid3i2dkdpfpsd814rgiwms54w4";
+    sha256 = "1hhxcpba4az72gdkznvr6gqvzxm7vfgmj0dz7il3dwv5mdfblphn";
   };
 
   buildFlagsArray = [
@@ -20,7 +20,7 @@ buildGoModule rec {
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  vendorSha256 = "0679aimi9aj52zsh81z0sa017l5s3ldyxi5rc5w8j9iw6slvnvxf";
+  vendorSha256 = "1adp037glz28fncycy8jp5sq7qas6x36amn6i6591zhzvs0c61is";
 
   meta = with stdenv.lib; {
     description = "A stream processor for mundane tasks written in Go";
