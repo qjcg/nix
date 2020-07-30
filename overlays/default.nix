8e84b07272a83889dae2011f6cb162ec5264582e
John Gosset <jgosset@drw.com>
2020-07-30 12:28:41 -0400

    Bump delve version to 1.5.0

--- overlays/default.nix
+++ overlays/default.nix
@@ -47,12 +47,12 @@ in {
   # Overrides
 
   delve = super.delve.overrideAttrs (oldAttrs: rec {
-    version = "1.4.0";
+    version = "1.5.0";
     src = self.fetchFromGitHub {
       owner = "go-delve";
       repo = "delve";
       rev = "v${version}";
-      sha256 = "1db74zw6a5kfjda1mcxqhvs4qcj5cvki2w66f6y0nvw3qgi4c4m4";
+      sha256 = "0m7fryclrj0qzqzcjn0xc9vl43srijyfahfkqdbm59xgpws67anp";
     };
   });
 
