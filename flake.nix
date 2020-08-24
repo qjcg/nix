9a53a97a37d9cf5b81756460a3c7a90518fce558
John Gosset <john@gossetx.com>
2020-08-24 17:01:38 -0400

    refactor: Continue evolving flake.nix

--- flake.nix
+++ flake.nix
@@ -6,18 +6,19 @@
     pkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
     pkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-20.03-darwin";
     pkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home.url = "github:rycee/home-manager/bqv-flakes";
     wayland.url = "github:colemickens/nixpkgs-wayland";
   };
 
-  outputs = { self, pkgs, pkgs-darwin, pkgs-unstable, wayland }:
+  outputs = { self, pkgs, pkgs-darwin, pkgs-unstable, home, wayland }:
 
     let
-      # Overlays, see https://nixos.wiki/wiki/Flakes#Importing_packages_from_multiple_channels
-      overlay-unstable = final: prev: {
-        unstable = pkgs-unstable.legacyPackages."x86_64-linux";
-      };
+      secrets = import ./secrets.nix;
 
-      overlay-wayland = final: prev: {
+      # Overlays, see https://nixos.wiki/wiki/Flakes#Importing_packages_from_multiple_channels
+      myOverlay = final: prev: {
+        home = home.legacyPackages."x86_64-linux";
+        unstable = pkgs-unstable.legacyPackages."x86_64-linux";
         wayland = wayland.legacyPackages."x86_64-linux";
       };
     in {
@@ -25,8 +26,17 @@
         system = "x86_64-linux";
 
         modules = [
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [ overlay-unstable overlay-wayland ];
+          ({ config, pkgs, ... }: {
+            nixpkgs.overlays = [ myOverlay ];
+            imports = [
+              (import ./machines/luban { inherit config pkgs secrets; })
+
+              (import ./roles/workstation-base { inherit pkgs; })
+              (import ./roles/workstation-gnome { inherit pkgs; })
+              (import ./roles/workstation-wayland { inherit pkgs; })
+
+              (import ./users/john.nix { inherit pkgs secrets; })
+            ];
           })
         ];
       };
