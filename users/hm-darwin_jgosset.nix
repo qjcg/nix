34770a9464ef5cb5722201a8b7e7508367477649
John Gosset <john@gossetx.com>
2020-08-25 23:54:43 -0400

    feat: Use Hyper color theme with alacritty

--- users/hm-darwin_jgosset.nix
+++ users/hm-darwin_jgosset.nix
@@ -19,8 +19,78 @@
           font.normal.style = "Regular";
           font.size = 20.0;
 
-          colors.cursor.text = "#000000";
-          colors.cursor.cursor = "#ef02f7";
+          colors = {
+            cursor = {
+              text = "#000000";
+              cursor = "#ef02f7";
+            };
+
+            # # Color Theme: Challenger Deep
+            # # Ref: https://github.com/eendroroy/alacritty-theme
+
+            # # Default colors
+            # primary = {
+            #   background = "0x1b182c";
+            #   foreground = "0xcbe3e7";
+            # };
+
+            # # Normal colors
+            # normal = {
+            #   black = "0x100e23";
+            #   red = "0xff8080";
+            #   green = "0x95ffa4";
+            #   yellow = "0xffe9aa";
+            #   blue = "0x91ddff";
+            #   magenta = "0xc991e1";
+            #   cyan = "0xaaffe4";
+            #   white = "0xcbe3e7";
+            # };
+
+            # # Bright colors
+            # bright = {
+            #   black = "0x565575";
+            #   red = "0xff5458";
+            #   green = "0x62d196";
+            #   yellow = "0xffb378";
+            #   blue = "0x65b2ff";
+            #   magenta = "0x906cff";
+            #   cyan = "0x63f2f1";
+            #   white = "0xa6b3cc";
+            # };
+
+            # Color Theme: Hyper
+            # Ref: https://github.com/eendroroy/alacritty-theme
+
+            # Default colors
+            primary = {
+              background = "0x000000";
+              foreground = "0xffffff";
+            };
+
+            # Normal colors
+            normal = {
+              black = "0x000000";
+              red = "0xfe0100";
+              green = "0x33ff00";
+              yellow = "0xfeff00";
+              blue = "0x0066ff";
+              magenta = "0xcc00ff";
+              cyan = "0x00ffff";
+              white = "0xd0d0d0";
+            };
+
+            # Bright colors
+            bright = {
+              black = "0x808080";
+              red = "0xfe0100";
+              green = "0x33ff00";
+              yellow = "0xfeff00";
+              blue = "0x0066ff";
+              magenta = "0xcc00ff";
+              cyan = "0x00ffff";
+              white = "0xFFFFFF";
+            };
+          };
         };
       };
 
