404c5e9a32efb9aa8475503e0310ff29bfd0036e
John Gosset <john@gossetx.com>
2020-09-30 22:28:25 -0400

    chore: Bump horeb package

--- packages/horeb/default.nix
+++ packages/horeb/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "horeb";
-  version = "0.11.1";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "qjcg";
@@ -11,7 +11,7 @@ buildGoModule rec {
 
     # To get this value, use "nix-prefetch-url --unpack" with the release tarball, eg:
     #   nix-prefetch-url --unpack https://github.com/qjcg/4d/archive/v0.5.5.tar.gz
-    sha256 = "0h08fmaf90i5arl8maxhffzh2xm3cd55xq58ym55zvba9yrjks0i";
+    sha256 = "sha256-lHTuiaRy2sgeKB838W8QQXiTGuoEG0izoIBp+4d2zmI=";
   };
 
   buildFlagsArray =
@@ -20,7 +20,7 @@ buildGoModule rec {
   # First, provide a fake hash via the value: lib.fakeSha256
   # Then, during build, copy "got" value in here.
   # Ref: https://discourse.nixos.org/t/how-to-create-modsha256-for-buildgomodule/3059/2
-  vendorSha256 = "1dgczgl8i1cpxmm0pws9258xnycwvksvpwvdkccmx4v5civ9ydlx";
+  vendorSha256 = "sha256-rmZGr6TcdDPBkyNRMY0vfUyiwoHLuGXGw+XKlVMzuH0=";
 
   meta = with stdenv.lib; {
     description = "Speaking in tongues via stdout";
