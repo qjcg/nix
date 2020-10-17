14fbb1083ba4e4154a90d3dd977921df967c7c4e
John Gosset <john@gossetx.com>
2020-10-17 15:41:56 -0400

    feat: Add container flake

--- flake.nix
+++ flake.nix
@@ -1,33 +1,60 @@
-# See https://www.tweag.io/blog/2020-07-31-nixos-flakes/
+# References:
+# - [Nix Flakes, Part 3: Managing NixOS Systems](https://www.tweag.io/blog/2020-07-31-nixos-flakes/)
+# - [NixOS: Modules](https://nixos.org/manual/nixos/stable/index.html#ex-module-syntax)
+# - [NixOS: Containers](https://nixos.org/manual/nixos/stable/#ch-containers)
+# - [Nixpkgs: Overlays](https://nixos.org/manual/nixpkgs/stable/#chap-overlays)
+# - [Wiki: Flakes > Input schema](https://nixos.wiki/wiki/Flakes#Input_schema)
+# - [Wiki: Flakes > Output schema](https://nixos.wiki/wiki/Flakes#Output_schema)
+
 {
   description = "A flake for my nix configurations";
 
   inputs = {
-    pkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-    pkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-20.03-darwin";
+    pkgs-stable.url = "github:NixOS/nixpkgs/nixos-20.03";
+    pkgs-stable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-20.03-darwin";
     pkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home.url = "github:rycee/home-manager/bqv-flakes";
+    home-manager.url = "github:nix-community/home-manager/bqv-flakes";
+    nur.url = "github:nix-community/NUR";
+    sops.url = "github:Mic92/sops-nix";
     wayland.url = "github:colemickens/nixpkgs-wayland";
   };
 
-  outputs = { self, pkgs, pkgs-darwin, pkgs-unstable, home, wayland }:
+  outputs = { self, ... }@inputs:
 
-    let
-      secrets = import ./secrets.nix;
-
-      # Overlays, see https://nixos.wiki/wiki/Flakes#Importing_packages_from_multiple_channels
-      myOverlay = final: prev: {
-        home = home.legacyPackages."x86_64-linux";
-        unstable = pkgs-unstable.legacyPackages."x86_64-linux";
-        wayland = wayland.legacyPackages."x86_64-linux";
-      };
+    let secrets = import ./secrets.nix;
     in {
-      nixosConfigurations.luban = pkgs.lib.nixosSystem {
+
+      # See:
+      #   - https://nixos.wiki/wiki/Flakes#Importing_packages_from_multiple_channels
+      overlays = {
+        personal = import ./overlays;
+
+        thirdParty = final: prev: {
+          home-manager = inputs.home-manager.legacyPackages."x86_64-linux";
+          unstable = inputs.pkgs-unstable.legacyPackages."x86_64-linux";
+        };
+      };
+
+      nixosConfigurations.containerTest = inputs.pkgs-unstable.lib.nixosSystem {
         system = "x86_64-linux";
 
         modules = [
           ({ config, pkgs, ... }: {
-            nixpkgs.overlays = [ myOverlay ];
+            boot.isContainer = true;
+
+            nixpkgs.overlays =
+              [ self.overlays.personal self.overlays.thirdParty ];
+            environment.systemPackages = with pkgs; [ benthos neovim ];
+          })
+        ];
+      };
+
+      nixosConfigurations.luban = inputs.pkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ({ config, pkgs, ... }: {
+            nixpkgs.overlays = [ self.overlays.thirdParty ];
             imports = [
               (import ./machines/luban { inherit config pkgs secrets; })
 
