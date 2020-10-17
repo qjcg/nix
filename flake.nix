63bcdacf3126f2359b3a1ba362f65c949fb2e302
John Gosset <john@gossetx.com>
2020-10-17 18:54:45 -0400

    docs: Tweak comment

--- flake.nix
+++ flake.nix
@@ -69,8 +69,8 @@
             # Let 'nixos-version --json' know about the Git revision of this flake.
             system.configurationRevision = pkgs.lib.mkIf (self ? rev) self.rev;
 
-            # Not needed/used in systemd-nspawn container? (To confirm).
             networking.hostName = "test";
+            # DHCP not needed/used in systemd-nspawn container? (To confirm).
             networking.useDHCP = false;
 
             # Create a normal user for testing home-manager.
