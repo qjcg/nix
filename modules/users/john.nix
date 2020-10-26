8fd5a6e824e5a49df90b3f2d0026517cd468f0a2
John Gosset <john@gossetx.com>
2020-10-26 09:58:32 -0400

    feat: Add adb settings for john user

--- modules/users/john.nix
+++ modules/users/john.nix
@@ -1,20 +1,27 @@
 { config, home-manager, pkgs, secrets, ... }:
 
 {
-
   imports = [ home-manager ];
 
   users.users.john = {
     description = "John Gosset";
     isNormalUser = true;
     uid = 7777;
-    extraGroups =
-      [ "audio" "disk" "docker" "networkmanager" "vboxusers" "wheel" ];
+    extraGroups = [
+      "adbusers"
+      "audio"
+      "disk"
+      "docker"
+      "networkmanager"
+      "vboxusers"
+      "wheel"
+    ];
   };
+
+  programs.adb.enable = true;
 
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
   home-manager.users.john =
     import ./john_hm.nix { inherit config pkgs secrets; };
-
 }
