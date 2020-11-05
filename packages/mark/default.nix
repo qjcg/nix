d76350f041cec52f90316a03d3984bd6cb4d948e
John Gosset <john@gossetx.com>
2020-11-05 11:05:14 -0500

    chore(mark): Bump package version

--- packages/mark/default.nix
+++ packages/mark/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "mark";
-  version = "3.3";
+  version = "3.3.1";
 
   src = fetchFromGitHub {
     owner = "kovetskiy";
     repo = "mark";
     rev = "${version}";
-    sha256 = "sha256-BvM9D3o1qPKrQZTUf1tu1OeC16eaoFE9bEVai3ScTDc=";
+    sha256 = "sha256-XMPzXXR33htWm0ptzAlzHFV4BNGQZhb1Fu2MV/vn+fc=";
   };
 
   subPackages = [ "." ];
-  vendorSha256 = "sha256-vDkL7Eel1D/n7X5NAM1n9DWhA1V3daeMmRuVsT0pKfY=";
+  vendorSha256 = "sha256-Y1qoklqC0N0m1w0958jN0msUm5OVwD2ad0MaXoH/3jw=";
 
   meta = with stdenv.lib; {
     description =
