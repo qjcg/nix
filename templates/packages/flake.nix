5d63bfbced0c7b0e93feb3f2118f2af52c59c75d
John Gosset <john@gossetx.com>
2020-11-04 19:59:32 -0500

    feat: Update packages template

--- templates/packages/flake.nix
+++ templates/packages/flake.nix
@@ -2,7 +2,7 @@
   description = "A flake providing single package (and setting defaultPackage).";
 
   inputs = {
-    pkgs.url = "github:nixos/nixpkgs/nixos-20.03";
+    pkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, pkgs }:
@@ -11,7 +11,7 @@
         with import pkgs { system = "x86_64-linux"; };
         stdenv.mkDerivation {
           name = "hello";
-          src = self;
+          src = ./src;
           buildPhase = "gcc -o hello ./hello.c";
           installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
         };
