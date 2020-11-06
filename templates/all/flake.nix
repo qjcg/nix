5b22e5f0f5b4c2eee388c2882a7dd8284bb83d14
John Gosset <john@gossetx.com>
2020-11-06 17:24:57 -0500

    feat: Add "all" template

---
+++ templates/all/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A kitchen sink flake, showing main functionality";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, ... }@inputs: {
+
+    nixosConfigurations.flakeimp = inputs.nixpkgs-unstable.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ../../modules/container.nix
+        ../../modules/simple.nix
+      ];
+    };
+
+  };
+}
