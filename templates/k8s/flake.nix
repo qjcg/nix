c4b05123af565cda1a79465b33505e88d0abebc0
John Gosset <john@gossetx.com>
2020-11-06 17:16:11 -0500

    refactor: Tweak k8s template flake

--- templates/k8s/flake.nix
+++ templates/k8s/flake.nix
@@ -2,19 +2,20 @@
   description = "A flake providing a k8s development shell.";
 
   inputs = {
-    pkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, ... }@inputs:
-    with inputs.flake-utils.lib;
-
-    eachDefaultSystem (system:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = inputs.pkgs.legacyPackages.${system};
+        pkgs = inputs.nixpkgs-unstable.legacyPackages.${system};
       in
       {
-        devShell = import ./shell.nix { inherit pkgs; useVim = true; };
+        devShell = import ./shell.nix {
+          inherit pkgs;
+          useVim = true;
+        };
       }
     );
 }
