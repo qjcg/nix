2614e197a0c5c426f4900d4d45246d3178c06206
John Gosset <john@gossetx.com>
2020-11-07 13:48:51 -0500

    feat: Add checks and fix path of package template

--- templates/package/flake.nix
+++ templates/package/flake.nix
@@ -11,10 +11,33 @@
       defaultPackage.x86_64-linux =
         with import pkgs { system = "x86_64-linux"; };
         stdenv.mkDerivation {
-          name = "hello";
+          name = "defaultpackage-hello";
           src = self;
-          buildPhase = "gcc -o hello ./hello.c";
+          buildPhase = "gcc -o hello " + ./hello.c;
           installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
         };
+
+      checks.x86_64-linux = {
+        build = self.defaultPackage.x86_64-linux;
+
+        # See https://nixos.org/manual/nixos/unstable/index.html#sec-nixos-tests
+        test = with import (pkgs + "/nixos/lib/testing-python.nix") { system = "x86_64-linux"; };
+
+          makeTest {
+            nodes = {
+              client = { ... }: {
+                # FIXME
+                #imports = [ self.defaultPackage.x86_64-linux ];
+              };
+            };
+
+            testScript = ''
+              start_all()
+              client.wait_for_unit("multi-user.target")
+              client.succeed("echo hello")
+              client.fail("echo999 hello")
+            '';
+          };
+      };
     };
 }
