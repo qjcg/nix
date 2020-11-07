c97b91161156525150cfe4b1b3b13d29d6448693
John Gosset <john@gossetx.com>
2020-11-07 14:07:14 -0500

    docs: Add comments to flake debug lib

--- lib/default.nix
+++ lib/default.nix
@@ -2,13 +2,20 @@ let
   inherit (builtins) getFlake trace;
 in
 {
+
+  # Namespace this "debugging session" inside a debug attr for easy
+  # discoverability via tab-completion.
+  # Use via `nix repl default.nix`.
   debug = rec {
-    nixpkgs-unstable = getFlake "github:nixos/nixpkgs/nixpkgs-unstable";
+    # The top-level flake in this repo.
     thisFlake = getFlake (toString ../.);
+
+    # Upstream nixpkgs flake.
+    nixpkgs-unstable = getFlake "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Demonstrate adding this flake's overlay to upstream nixpkgs via flake import.
     nixpkgsWithOverlay = import nixpkgs-unstable {
       overlays = [ thisFlake.overlay ];
     };
-
-    pk = p: nixpkgsWithOverlay.packages.${p} null;
   };
 }
