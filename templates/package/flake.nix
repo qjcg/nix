c9fb9a6c22c3b19b3f2fc14c9ba2b2185078208d
John Gosset <john@gossetx.com>
2020-11-07 15:14:57 -0500

    feat: Make package template cross-platform

--- templates/package/flake.nix
+++ templates/package/flake.nix
@@ -3,41 +3,49 @@
   description = "A flake providing single package (and setting defaultPackage).";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     pkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, pkgs }:
-    {
-      defaultPackage.x86_64-linux =
-        with import pkgs { system = "x86_64-linux"; };
-        stdenv.mkDerivation {
-          name = "defaultpackage-hello";
-          src = self;
-          buildPhase = "gcc -o hello " + ./hello.c;
-          installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
-        };
-
-      checks.x86_64-linux = {
-        build = self.defaultPackage.x86_64-linux;
-
-        # See https://nixos.org/manual/nixos/unstable/index.html#sec-nixos-tests
-        test = with import (pkgs + "/nixos/lib/testing-python.nix") { system = "x86_64-linux"; };
-
-          makeTest {
-            nodes = {
-              client = { ... }: {
-                # FIXME
-                #imports = [ self.defaultPackage.x86_64-linux ];
-              };
-            };
-
-            testScript = ''
-              start_all()
-              client.wait_for_unit("multi-user.target")
-              client.succeed("echo hello")
-              client.fail("echo999 hello")
-            '';
+  outputs = { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.pkgs { system = "${system}"; };
+      in
+      {
+        defaultPackage =
+          with pkgs;
+          stdenv.mkDerivation {
+            name = "defaultpackage-hello";
+            src = self;
+            nativeBuildInputs = [ gcc ];
+            buildPhase = "gcc -o hello " + ./hello.c;
+            installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
           };
-      };
-    };
+
+        checks = {
+          build = self.defaultPackage.${system};
+
+          # NOTE: Below test fails to run on darwin.
+        } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+
+          # See https://nixos.org/manual/nixos/unstable/index.html#sec-nixos-tests
+          test = with import (inputs.pkgs + "/nixos/lib/testing-python.nix") { system = "${system}"; };
+
+            makeTest {
+              nodes = {
+                client = { ... }: {
+                  # FIXME
+                  #imports = [ self.defaultPackage.x86_64-linux ];
+                };
+              };
+
+              testScript = ''
+                start_all()
+                client.wait_for_unit("multi-user.target")
+                client.succeed("echo hello")
+              '';
+            };
+        };
+      });
 }
