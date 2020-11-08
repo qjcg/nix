7426e9bcfb9dc1d695896222ff7ee927b6870fd2
John Gosset <john@gossetx.com>
2020-11-08 07:45:12 -0500

    fix: Emacs package now uses emacs config file

--- packages/custom/emacs/default.nix
+++ packages/custom/emacs/default.nix
@@ -1,19 +1,6 @@
-# Inspired by: https://github.com/peel/dotfiles/blob/master/overlays/20-emacs/emacs/default.nix
+{ pkgs }:
 
-{ pkgs, }:
-
+# See https://github.com/nix-community/emacs-overlay/#extra-library-functionality
 pkgs.emacsWithPackagesFromUsePackage {
-  config = builtins.readFile ../../../files/emacs/init.el;
-
-  # TODO: Figure out how to deploy my personal init.el via this overlay (i.e. avoiding home-manager).
-  #override = epkgs: epkgs // {
-  #  myConfig = (pkgs.runCommand "init.el" {} ''
-  #           mkdir -p $out/share/emacs/site-lisp
-  #           cp -r ${myConfig} $out/share/emacs/site-lisp/init.el
-  #  '');
-  #};
-  #extraEmacsPackages = epkgs: with epkgs; [
-  #] ++ [
-  #  myConfig
-  #];
+  config = ../../../files/emacs/init.el;
 }
