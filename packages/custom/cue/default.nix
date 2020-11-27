6f261c4e482bb50904e04d95cf216a5c313e39f5
John Gosset <john@gossetx.com>
2020-11-27 12:12:32 -0800

    chore(cue): Bump package version

--- packages/custom/cue/default.nix
+++ packages/custom/cue/default.nix
@@ -2,15 +2,15 @@
 
 buildGoModule rec {
   pname = "cue";
-  version = "0.3.0-alpha4";
+  version = "0.3.0-alpha5";
 
   src = fetchgit {
     url = "https://cue.googlesource.com/cue";
     rev = "v${version}";
-    sha256 = "0ajff3c6zkzbm0n5982d0ijwy6zflf12df9g8xglx6a0pvy9idbc";
+    sha256 = "sha256-Dh5BnVpoTZ44oiOtSVUgdp/qZ3ckApXW4hAJPTCvelo=";
   };
 
-  vendorSha256 = "136igsvw1x96rf8vlpzw325y0pfjxypy3dbc8wji3my9s5w9267h";
+  vendorSha256 = "sha256-t03E/KGhwruyF39NJSeHhum3JprTUErMoOe3ixuvuXU=";
 
   doCheck = false;
 
