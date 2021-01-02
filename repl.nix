602f95421aac6634f8f6cb7986a5918ef240f951
John Gosset <john@gossetx.com>
2021-01-02 17:38:50 -0500

    feat: Add repl.nix to top-level dir

---
+++ repl.nix
@@ -0,0 +1,27 @@
+# USAGE: nix repl repl.nix
+let
+  inherit (builtins) getFlake trace;
+in
+{
+
+  # Use the "repl" attrset for easy discoverability via tab-completion.
+  repl = rec {
+    # The top-level flake in this repo.
+    thisFlake = getFlake (toString ./.);
+
+    # Upstream nixpkgs flake.
+    nixpkgs-unstable = getFlake "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Demonstrate adding this flake's top-level "overlay" to upstream nixpkgs via flake import.
+    nixpkgsWithOverlay = import nixpkgs-unstable {
+      overlays = [ thisFlake.overlay ];
+    };
+
+    # Demonstrate adding this flake's attrset overlays to upstream nixpkgs via flake import.
+    nixpkgsWithOverlays = {
+      main = import nixpkgs-unstable {
+        overlays = [ thisFlake.overlays.main ];
+      };
+    };
+  };
+}
