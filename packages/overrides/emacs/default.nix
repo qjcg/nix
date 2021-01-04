4999808f3d5c03bd49a7d675e65dc92e1c0ffaf2
John Gosset <john@gossetx.com>
2021-01-04 17:11:31 -0500

    docs(emacs): Add comments to override package

--- packages/overrides/emacs/default.nix
+++ packages/overrides/emacs/default.nix
@@ -3,5 +3,10 @@
 # See https://github.com/nix-community/emacs-overlay/#extra-library-functionality
 pkgs.emacsWithPackagesFromUsePackage {
   config = ../../../files/emacs/init.org;
+
+  # Set to `false` to ONLY pull in packages with `:ensure`.
+  alwaysEnsure = false;
+
+  # Since I have `#+PROPERTY: header-args: emacs-lisp :tangle yes` at the top of init.org.
   alwaysTangle = true;
 }
