fd52333405afd4a522b2288bb77512d87c542e18
John Gosset <john@gossetx.com>
2021-01-05 09:45:34 -0500

    chore(k3d): Change k3sVersion

--- packages/custom/k3d/default.nix
+++ packages/custom/k3d/default.nix
@@ -1,8 +1,12 @@
-{ lib, fetchFromGitHub, buildGoModule, installShellFiles,
-
-  # NOTE: K3s docker images use a "-k3s" suffix, while the k3d repo uses a "+k3d" suffix in git tags.
+{ lib
+, fetchFromGitHub
+, buildGoModule
+, installShellFiles
+, # NOTE: K3s docker images use a "-k3s" suffix, while the k3d repo uses a "+k3d" suffix in git tags.
   # See also https://update.k3s.io/v1-release/channels/stable
-  k3sVersion ? "v1.18.12-k3s2",
+  # (version selected here should correspond to the k8s version used in prod)
+  k3sVersion ? "v1.18.6-k3s1"
+,
 }:
 
 with lib;
