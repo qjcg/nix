b8526337473ae2eb67cbb01722c2656f8f151eeb
John Gosset <john@gossetx.com>
2021-01-05 09:27:53 -0500

    chore(benthos): Bump package version

--- packages/custom/benthos/default.nix
+++ packages/custom/benthos/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "benthos";
-  version = "3.35.0";
+  version = "3.36.0";
 
   src = fetchFromGitHub {
     owner = "Jeffail";
     repo = "benthos";
     rev = "v${version}";
-    sha256 = "sha256-rQ9IF6Wf2E6t6aB35vjtpIu60nXgkunbVcA40pL7gso=";
+    sha256 = "sha256-QQRgcamvNvo/wXalrMltShyKFGChWJmLKsZ1taMudSk=";
   };
 
-  vendorSha256 = "sha256-atgn0jIwMD3aKIDbdKNHi4bB5sASqOnyqTLl0P9g2U8=";
-  
+  vendorSha256 = "sha256-6UszFj73dZwyoOvxgBiGfrXqjKDZPc1o7IKaDl4sk30=";
+
   buildFlagsArray = [
     "-ldflags="
     "-X github.com/Jeffail/benthos/v3/lib/service.Version=${src.rev}"
