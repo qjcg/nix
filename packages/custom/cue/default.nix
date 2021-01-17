1dab92144173326fdbc5c3b306e9359f5fa4e9d0
John Gosset <john@gossetx.com>
2021-01-17 10:55:43 -0500

    feat(packages): Bump cue version

--- packages/custom/cue/default.nix
+++ packages/custom/cue/default.nix
@@ -1,13 +1,15 @@
 { buildGoModule, fetchgit, stdenv }:
-
+let
+  inherit (stdenv.lib) fakeSha256;
+in
 buildGoModule rec {
   pname = "cue";
-  version = "0.3.0-alpha6";
+  version = "0.3.0-beta.2";
 
   src = fetchgit {
     url = "https://cue.googlesource.com/cue";
     rev = "v${version}";
-    sha256 = "sha256-GhwfuC8Hb/LzPOvwB+VNcQxkB9hz0xxKJXWqKyJmIfg=";
+    sha256 = "sha256-/r+pqIysXiLp0EgPKuoGV/c6bIZ/kKG1zC1lYl6CnZo=";
   };
 
   vendorSha256 = "sha256-t03E/KGhwruyF39NJSeHhum3JprTUErMoOe3ixuvuXU=";
