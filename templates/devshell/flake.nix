dfc5ca1fca9f19fa47ef964a67d69b9626d1325d
John Gosset <john@gossetx.com>
2021-01-21 06:59:36 -0500

    feat(templates): Use mkDevShell in devshell flake

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -1,31 +1,47 @@
+# NOTE: Update flake.nix to use devshell as done below!
 {
   description = "A flake providing a development shell.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    devshell.url = "github:numtide/devshell";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    qjcg.url = "github:qjcg/nix";
   };
 
   outputs = { self, ... }@inputs:
+    let
+      pkgs = import inputs.nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          inputs.devshell.overlay
+          inputs.qjcg.overlay
+        ];
+      };
+    in
     {
       devShell.x86_64-linux = (
         { pkgs, ... }:
 
           with pkgs;
 
-          mkShell {
-            name = "devshell-myapp";
-            buildInputs = [ hello ];
+          mkDevShell {
+            name = "devshell-financial";
+            packages = [
+              jg.envs.env-financial
+              jg.overrides.neovim
+              zathura
+            ];
+
             shellHook = ''
-              cat << END
-
-              Nix shell from a flake! To use, run "nix develop"
-
-              Is this AWESOME? -> $AWESOME
-
-              END
+              # Demonstrate creating a custom shell function.
+              func awesome() {
+                echo awesome
+              }
             '';
+
             AWESOME = "Yes indeed!";
+
           }
-      ) { pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux; };
+      ) { inherit pkgs; };
     };
 }
