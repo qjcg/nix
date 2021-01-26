6be3ae6246c0a3541cd056d44fedeb80a5b14454
John Gosset <john@gossetx.com>
2021-01-26 12:45:54 -0500

    feat(sbagen): Add multi-arch package

---
+++ packages/newer/sbagen/default.nix
@@ -0,0 +1,33 @@
+{ lib, multiStdenv, fetchurl }:
+let
+  inherit (lib) fakeSha256;
+in
+multiStdenv.mkDerivation rec {
+  pname = "sbagen";
+  version = "1.4.5";
+
+  src = fetchurl {
+    url = "https://uazu.net/sbagen/${pname}-${version}.tgz";
+    sha256 = "sha256-ArBdD4nxuqbmsoL0pdsnm0xZ7m/EAKWpaGqhEofyIOQ=";
+  };
+
+  postPatch = ''
+    patchShebangs ./mk
+  '';
+
+  buildPhase = "./mk";
+
+  installPhase = ''
+    mkdir -p $out/{bin,share/sbagen/doc}
+    cp -r --target-directory=$out/share/sbagen examples scripts river1.ogg river2.ogg
+    cp sbagen $out/bin
+    cp --target-directory=$out/share/sbagen/doc README.txt SBAGEN.txt theory{,2}.txt {wave,holosync,focus,TODO}.txt
+  '';
+
+  meta = {
+    description = "Binaural sound generator";
+    homepage = "http://uazu.net/sbagen";
+    license = lib.licenses.gpl2;
+    platforms = [ "i686-linux" "x86_64-linux" ];
+  };
+}
