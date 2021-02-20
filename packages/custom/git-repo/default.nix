b5b0a61ca0e52025a511b993c0a3a3443d7bfe3f
John Gosset <john@gossetx.com>
2021-02-19 23:16:33 -0500

    feat(packages): Add git-repo package

---
+++ packages/custom/git-repo/default.nix
@@ -0,0 +1,31 @@
+{ stdenv, fetchFromGitHub, buildGoModule, }:
+let
+  inherit (stdenv.lib) fakeSha256;
+  inherit (stdenv.lib.licenses) asl20;
+
+  version = "0.7.7";
+in
+buildGoModule {
+  inherit version;
+  pname = "git-repo";
+
+  src = fetchFromGitHub {
+    owner = "alibaba";
+    repo = "git-repo-go";
+    rev = "v${version}";
+    sha256 = "sha256-SG0e1wztabfhVXjn01u72Lc6Yyxqiq8uc3r1GxB9r1g=";
+  };
+
+  subPackages = [ "." ];
+  vendorSha256 = "sha256-rIk6qPJ3XSMgroHaMyxpjjUrejG/jlvNL7MwW/V2HzQ=";
+
+  postFixup = ''
+    mv $out/bin/git-repo-go $out/bin/git-repo
+  '';
+
+  meta = {
+    description = "An easy-to-use solution for multiple repositories";
+    homepage = "https://github.com/alibaba/git-repo-go";
+    license = asl20;
+  };
+}
