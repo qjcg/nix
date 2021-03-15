9be011d2b8b32407ba6e4cabe669d7019e2a1003
John Gosset <john@gossetx.com>
2021-03-14 23:29:03 -0400

    refactor(modules): Add common/environment.nix

---
+++ modules/common/environment.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  config =
+    {
+
+      # The following base config is always applied when this role is enabled.
+      environment.systemPackages = with pkgs.jg.envs; [
+        env-go
+        env-k8s
+        env-multimedia
+        env-nix
+        env-personal
+        env-python
+        env-tools
+      ];
+
+      environment.variables.EDITOR = "nvim";
+      environment.variables.PAGER = "less";
+      environment.variables.VISUAL = "nvim";
+    };
+}
