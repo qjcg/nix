572e17f4608e90d0e18ac15850c4a97ef4fdcc1c
John Gosset <john@gossetx.com>
2021-03-23 16:16:07 -0400

    chore: Bump cue package

--- packages/newer/cue/default.nix
+++ packages/newer/cue/default.nix
@@ -2,7 +2,7 @@
 let
   inherit (pkgs) buildGoModule fetchgit;
   inherit (pkgs.lib) fakeSha256 licenses maintainers;
-  version = "0.3.0-beta.6";
+  version = "0.3.0-beta.7";
 in
 buildGoModule {
   inherit version;
@@ -11,10 +11,10 @@ buildGoModule {
   src = fetchgit {
     url = "https://cue.googlesource.com/cue";
     rev = "v${version}";
-    sha256 = "sha256-2yEpYGqtvJPfZuHvX/4Ld/20IBqfqXIT63PnWah2ygk=";
+    sha256 = "sha256-0jsBewcaYJNRSmQSDxf8fmp1EM0rqtSwg25ll3y23AY=";
   };
 
-  vendorSha256 = "sha256-9ai1Wbk6ftcXHjVEWaL8drxZnhgAwF8+OXNI95CrNjc=";
+  vendorSha256 = "sha256-d8p/vsbJ/bQbT2xrqqCoU1sQB8MrwKOMwEYhNYTWe4I=";
 
   doCheck = false;
 
