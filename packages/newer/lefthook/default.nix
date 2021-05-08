15e983591e84eb2b13d80c9d173121b6b79ce37b
John Gosset <john@gossetx.com>
2021-05-08 10:57:32 -0400

    fix(lefthook): Update sha256

--- packages/newer/lefthook/default.nix
+++ packages/newer/lefthook/default.nix
@@ -14,7 +14,7 @@ buildGoModule {
     rev = "master";
     owner = "Arkweid";
     repo = "lefthook";
-    sha256 = "sha256-cSqwUf8KSr5uwXmlKbVUYqw+QJOsqb9P4N7Dkvzb39Q=";
+    sha256 = "sha256-DLBFbWaOJYdCBIM2NeendceeWgVfDnU7EeCpPD574bA=";
   };
 
   vendorSha256 = "sha256-XR7xJZfgt0Hx2DccdNlwEmuduuVU8IBR0pcIUyRhdko=";
