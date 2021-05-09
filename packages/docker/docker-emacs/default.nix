892736eb9d9fdfc1c6405bf2b4a0be2e012bb04b
John Gosset <john@gossetx.com>
2021-05-09 09:55:07 -0400

    feat: Add busybox to docker-emacs

--- packages/docker/docker-emacs/default.nix
+++ packages/docker/docker-emacs/default.nix
@@ -1,13 +1,15 @@
 { pkgs }:
 let
   inherit (pkgs) dockerTools;
-  inherit (pkgs.jg.overrides) emacs;
 in
 dockerTools.buildImage {
   name = "emacs";
   tag = "latest";
   created = "now";
-  contents = emacs;
+  contents = with pkgs; [
+    busybox
+    jg.overrides.emacs
+  ];
 
   config = {
     cmd = [ "/bin/emacs" ];
