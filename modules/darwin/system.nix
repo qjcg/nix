23896d8ec4390c70b698d38b432c4c916784d327
John Gosset <john@gossetx.com>
2021-07-16 09:00:36 -0400

    fix(nix): Reset KeyRepeat (avoids bad behavior)

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -3,6 +3,6 @@
 {
   config = {
     system.defaults.NSGlobalDomain.InitialKeyRepeat = null; # This sets how long you must hold down the key before it starts repeating.
-    system.defaults.NSGlobalDomain.KeyRepeat = 0; # Sets how fast keys repeat once repeating has started.
+    system.defaults.NSGlobalDomain.KeyRepeat = null; # Sets how fast keys repeat once repeating has started.
   };
 }
